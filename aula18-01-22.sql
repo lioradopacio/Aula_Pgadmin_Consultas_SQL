8db76c41064b9a6f0118b3d00c03e1194e40dc0b
lioradopacio <liora.vanessa@gmail.com>
2022-01-18 20:26:57 -0300

    Exercícios extras de SQL sobre o banco de dados pizzaria

---
+++ aula18-01-22.sql
@@ -0,0 +1,35 @@
+-- Selecionar nome de todos os clientes, associados com seu pedido
+-- nome das pizzas e clientes que não pediram nenhuma pizza
+select tb_cliente.nome, tb_pedido_pizza.id_pedido, tb_pizza.nome 
+from tb_cliente
+left join tb_pedido on tb_pedido.id_cliente = tb_cliente.id_cliente
+left join tb_pedido_pizza on tb_pedido_pizza.id_pedido = tb_pedido.id_pedido
+left join tb_pizza on tb_pizza.id_pizza = tb_pedido_pizza.id_pizza
+order by tb_pedido_pizza.id_pedido
+
+-- Selecionar o nome de todos os clientes que dizeram pedidos
+-- valor gasto por cada um até hoje na pizzaria
+select tb_cliente.nome, sum(tb_pedido.preco) as valor_total_gasto
+from tb_cliente
+inner join tb_pedido on tb_pedido.id_cliente = tb_cliente.id_cliente
+group by tb_cliente.nome
+order by sum(tb_pedido.preco);
+
+--Valor em vendas de todas as pizzas zero lactose
+select tb_pizza.nome, sum(tb_pedido.preco) from tb_pizza
+inner join tb_pedido_pizza on tb_pedido_pizza.id_pizza = tb_pizza.id_pizza
+inner join tb_pedido on tb_pedido.id_pedido = tb_pedido_pizza.id_pedido
+where lower (nome) like '%zero%'
+group by tb_pizza.nome;
+
+-- Valor e quantidade vendas pizzas zero
+select tb_pizza.nome, sum(tb_pedido.preco), count(tb_pizza.nome) from tb_pizza
+inner join tb_pedido_pizza on tb_pedido_pizza.id_pizza = tb_pizza.id_pizza
+inner join tb_pedido on tb_pedido.id_pedido = tb_pedido_pizza.id_pedido
+where lower (nome) like '%zero%'
+group by tb_pizza.nome;
+
+
+
+
+
